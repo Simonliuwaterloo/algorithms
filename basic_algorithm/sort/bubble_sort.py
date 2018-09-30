95eef0102953f339450f2c2967a170fd2c1fc3b4
Simon Li <simonli1215@outlook.com>
2018-09-29 23:33:16 -0400

    make the two algorithms proper functions

--- basic_algorithm/sort/bubble_sort.py
+++ basic_algorithm/sort/bubble_sort.py
@@ -2,19 +2,19 @@
 import pandas as pd
 import numpy as np
 
+def bubble_sort(data_name):
+    raw_data = list()
+    for n in range(0,len(list(pd.read_csv(data_name)))):
+        raw_data.append(int(list(pd.read_csv(data_name))[n]))
 
-raw_data = list()
-for n in range(0,len(list(pd.read_csv('data.txt')))):
-    raw_data.append(int(list(pd.read_csv('data.txt'))[n]))
 
 
-
-count = 0
-while count < len(raw_data) + 1:
-    for n in range(1, len(raw_data)):
-        if raw_data[-n] < raw_data[-n - 1]:
-            tmp = raw_data[-n]
-            raw_data[-n] = raw_data[-n - 1]
-            raw_data[-n - 1] = tmp
-    count = count + 1
-print(raw_data)
+    count = 0
+    while count < len(raw_data) + 1:
+        for n in range(1, len(raw_data)):
+            if raw_data[-n] < raw_data[-n - 1]:
+                tmp = raw_data[-n]
+                raw_data[-n] = raw_data[-n - 1]
+                raw_data[-n - 1] = tmp
+        count = count + 1
+    return(raw_data)
--- basic_algorithm/sort/selection_sort.py
+++ basic_algorithm/sort/selection_sort.py
@@ -2,24 +2,21 @@
 import pandas as pd
 import numpy as np
 
+def selection_sort(data_name):
+    raw_data = list()
+    for n in range(0,len(list(pd.read_csv(data_name)))):
+        raw_data.append(int(list(pd.read_csv(data_name))[n]))
 
-raw_data = list()
-for n in range(0,len(list(pd.read_csv('data.txt')))):
-    raw_data.append(int(list(pd.read_csv('data.txt'))[n]))
-print (raw_data)
-print (type(raw_data))
+    for m in range(0, len(raw_data)):
+        a = raw_data[m]
+        for n in range(m, len(raw_data)):
+            if raw_data[n] < a:
+                a = raw_data[n]
 
-for m in range(0, len(raw_data)):
-    a = raw_data[m]
-    for n in range(m, len(raw_data)):
-        if raw_data[n] < a:
-            a = raw_data[n]
-        print(a)
+        i = raw_data[m]
 
-    i = raw_data[m]
+        c = raw_data.index(a)
 
-    c = raw_data.index(a)
-
-    raw_data[m] = a
-    raw_data[c] = i
-    print(raw_data)
+        raw_data[m] = a
+        raw_data[c] = i
+    return(raw_data)
